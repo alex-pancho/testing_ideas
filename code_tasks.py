c863ce9d16c8281e0dd015c83677cc89687f3feb
Oleksandr Panchenko <pan.pan.4it@gmail.com>
2025-01-02 12:13:35 +0200

    used for count

--- code_tasks.py
+++ code_tasks.py
@@ -1,5 +1,4 @@
-
-input_string = "jklsdfjdkfadjkuiueuwewuh jschjkeuewuf/ UYEUWHJNBX"
+input_string = "jklsdfjdkfadjkuiueuwewuh jschjkeuewuf/ UYEUWHJNBX oooooooooo"
 
 def volwes_counter(incoming:str):
     count = 0
@@ -7,11 +6,9 @@ def volwes_counter(incoming:str):
     count_str = incoming.lower().strip()
     volwes = "aeyuio"
     for char in count_str:
-        if char in volwes and char not in count_dict:
-            count_dict[char] = 1
-        elif char in volwes and char  in count_dict:
-            count_dict[char] += 1
-        
+        if char in volwes:
+            count_dict[char] = count_dict[char] + 1 if count_dict.get(char, False) else 1      
+  
     return count_dict if count_dict else count
 
-print(volwes_counter("sdfghjk"))
+print(volwes_counter(input_string))
